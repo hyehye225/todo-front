8c0f8f5da30c9be6f55319d5581a8170a9bedcb6
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-03-31 09:57:49 +0900

    9_1. React에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import React from "react";
 import Hello from "./Hello";
+import { Paper, List } from "@material-ui/core";
 import Todo from "./Todo";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: { id: 0, title: "Hello World 1 ", done: true },
+      items: [
+        { id: 0, title: "Hello World 1 ", done: true },
+        { id: 1, title: "Hello World 2 ", done: false },
+      ],
     };
   }
   render() {
-    return (
-      <div className="App">
-        <Todo item={this.state.item} />
-      </div>
-    );
+    var todoItems = this.state.items.map((item, idx) => (
+      <Todo item={item} key={item.id} />
+    ));
+    return <div className="App">{todoItems}</div>;
   }
 }
 
