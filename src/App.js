1cef8b023ddd0216233d974a858e3eb1e464f9b4
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-03-31 13:27:50 +0900

    11. React로 목록 추가,수정,삭제하기

--- src/App.js
+++ src/App.js
@@ -22,12 +22,19 @@ class App extends React.Component {
     this.setState({ items: thisItems });
     console.log("items:", this.state.items);
   };
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items :", this.state.items);
+    });
+  };
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
